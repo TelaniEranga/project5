2faf5dfe482facbd033b61e6c2e3d84f795eade7
TelaniEranga <telanieranga@gmail.com>
2024-04-01 15:16:10 +0530

    devlops

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,44 +1,20 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
     </q-header>
+    <q-toolbar class="bg-primary text-white bg-black  shadow-2">
+      <q-btn flat round dense icon="menu" class="q-mr-sm" />
+      <q-separator dark vertical inset />
+      <q-btn stretch flat label="Link" />
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+      <q-space />
 
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
+      <q-separator dark vertical />
+      <q-btn stretch flat label="About" a href="HomePage.Vue" />
+      <q-separator dark vertical />
+      <q-btn stretch flat label="Contact Us" />
+    </q-toolbar>
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -46,71 +22,5 @@
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
-
-export default defineComponent({
-  name: 'MainLayout',
-
-  components: {
-    EssentialLink
-  },
-
-  setup () {
-    const leftDrawerOpen = ref(false)
-
-    return {
-      linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-})
 </script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,94 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
+  <q-page>
+    <!-- Hero Section -->
+    <q-banner
+      class="hero-section"
+      style="background-image: url('path_to_your_image.jpg')"
+      :dark="true"
     >
+      <div class="text-white">
+        <h1>Welcome to Our Website</h1>
+        <p>Explore our amazing features</p>
+      </div>
+    </q-banner>
+
+    <!-- Description Section -->
+    <div class="q-my-md q-pa-md">
+      <h2 class="text-h6">About Us</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
+      </p>
+    </div>
+
+    <!-- Card Section -->
+    <div class="q-my-md q-pa-md">
+      <h2 class="text-h6">Our Services</h2>
+      <q-card
+        class="q-my-md"
+        v-for="(service, index) in services"
+        :key="index"
+      >
+        <q-card-section>
+          <q-item
+            clickable
+            tag="a"
+            :href="service.link"
+            class="text-primary"
+          >
+            <q-item-section>
+              <q-item-label>{{ service.title }}</q-item-label>
+              <q-item-label caption>{{ service.description }}</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-card-section>
+      </q-card>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'IndexPage'
-})
+export default {
+  data () {
+    return {
+      services: [
+        {
+          title: 'Service 1',
+          description: 'Description of Service 1',
+          link: '#'
+        },
+        {
+          title: 'Service 2',
+          description: 'Description of Service 2',
+          link: '#'
+        },
+        {
+          title: 'Service 3',
+          description: 'Description of Service 3',
+          link: '#'
+        }
+      ]
+    }
+  }
+}
 </script>
+
+<style scoped>
+.hero-section {
+  height: 300px;
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.q-my-md {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+.q-pa-md {
+  padding: 20px;
+}
+</style>
